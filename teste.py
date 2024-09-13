f0cd2569f978a88085e9c9b32322bbbf3c2473e4
Bruno Martins <158478119+bruno-mar@users.noreply.github.com>
2024-09-13 11:14:18 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,31 @@
+print("Seja muito bem vindo ao quiz do Iury!")
+answer_user = input("Quer começar? (S/N) ")
+
+if answer_user != "S":
+    quit()
+
+score = 0
+
+
+print("Começando...")
+print("Quem desenvolveu o jogo Grand Theft Auto (GTA)? \n (A) Rockstar Games \n (B) Ubisoft \n (C) Activision \n (D) EA \n")
+answer_1 = input("Resposta: ")
+
+if answer_1 == "A":
+    print("Correto!")
+    score = score + 1
+
+else:
+    print("Incorreto")
+    
+print("Qual o protagonista do GTA San Andreas? \n (A) Carlos John \n (B) Carl jonhson \n (C) Carlos Jonhson \n (D) Carl jaqueline \n")
+answer_2 = input("Resposta: ")
+
+if answer_2 == "B":
+    print("Correto!")
+    score = score + 1
+
+else:
+    print("Incorreto")
+
+print(f"Quiz acabou....Pontuação: {score}/2")    
